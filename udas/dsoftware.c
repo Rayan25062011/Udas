5d86f762f8e1f08e095be3f40b7f642f7cdf6323
Rayan25062011 <101386337+Rayan25062011@users.noreply.github.com>
2023-02-07 10:53:36 +0200

    Update dsoftware.c

--- udas/dsoftware.c
+++ udas/dsoftware.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
  
 int main() {
-    if (remove("udas/client.rb", "udas/server.rb", "LICENSE", "README.md", "CODE_OF_CONDUCT.md", ".gitignore", "udas/database.sql") == 0) {
+    if (remove("udas/client.rb", "udas/server.rb", "LICENSE", "README.md", "CODE_OF_CONDUCT.md", ".gitignore", "udas/database.sql", "udas/test", "SECURITY.md") == 0) {
         printf("Software deleted.");
     } else {
         printf("Software could not have been deleted!");
